ada3f505eeafbd8aed92bfda01ae0bd3eefea557
Jinrae Kim <kjl950403@gmail.com>
2021-04-28 18:33:45 +0900

    잘못 구현된 LiP 고장 모델 수정 (#21) (#22)
    * Fix a bug of LiP fault model
    
    * Reflect seong-hun's comment
    
    * Fix a bug of LiP for variable step numerical integration
    
    * Change get method for LiP; linear interpolation
    
    * Correct comment

--- faults/faults.py
+++ faults/faults.py
@@ -1,5 +1,6 @@
 import numpy as np
 from copy import deepcopy
+import sys
 
 
 ## Fault models
@@ -75,21 +76,32 @@ A fault class for lock-in-place (LiP).
 # Parameters
 time: from when LoE is applied
 index: actuator index to which LoE is applied
-u_locked: locked value
 """
 class LiP(Fault):
-    def __init__(self, time=0, index=0, u_locked=0, name="LiP"):
+    def __init__(self, time=0, index=0, dt=0.00, name="LiP"):
+        if dt < 0 or dt < sys.float_info.epsilon:  # negative or too small
+            raise ValueError("Invalid time step to detect actuator value at the fault time")
         super().__init__(time=time, index=index, name=name)
-        self.u_locked = u_locked
+        self.dt = dt
+        self.memory = None
 
     def __repr__(self):
         _str = super().__repr__()
-        return _str + f", u_locked = {self.u_locked}"
+        return _str + f", dt = {self.dt}"
 
     def get(self, t, u):
+        if self.memory is None or t > self.memory[0]:
+            if t <= self.time:
+                self.memory = (t, u[self.index], False)
+            elif not self.memory[2]:
+                # Linear interpolation
+                dudt = (u[self.index] - self.memory[1]) / (t - self.memory[0])
+                u0 = self.memory[1] + dudt * (self.time - self.memory[0])
+                self.memory = (self.time, u0, True)
+
         u_fault = deepcopy(u)
-        if t >= self.time:
-            u_fault[self.index] = self.u_locked
+        if t > self.time:
+            u_fault[self.index] = self.memory[1]
         return u_fault
 
 
@@ -116,17 +128,27 @@ if __name__ == "__main__":
     def test(fault):
         print(fault)
         n = 6
-        ts = [0.0, 1.0, 2.0, 3.0]
-        u = np.ones(n)
+        ts = [0, 1.99, 2.0, 2.01, 10]
         for t in ts:
+            u = t*np.ones(n)
             print(f"Actuator command: {u}, time: {t}")
             u_fault = fault(t, u)
             print(f"Actual input: {u_fault}")
     # LoE
     faults = [
-        LoE(time=2, index=1, level=0.2),
+        LoE(time=2, index=1, level=0.1),
         Float(time=2, index=1),
-        LiP(time=2, index=1, u_locked=0.1),
+        LiP(time=2, index=1, dt=0.01),
     ]
     for fault in faults:
         test(fault)
+
+    # small test
+    def small_test():
+        fault = LiP(2, 0, 0.1)
+        print(fault.get(1.9, [3, 0]))  # 3
+        print(fault.get(1.95, [4, 1]))  # 4
+        print(fault.get(2.05, [5, 2]))  # 4.5
+        print(fault.get(1.96, [6, 3]))  # 6
+        print(fault.get(2.06, [7, 4]))  # 4.5
+    small_test()
