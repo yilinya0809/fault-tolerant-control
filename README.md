37ecfd282bf005090f065151bf81291e196aec0b
Hanna Lee <dlgkssk707@gmail.com>
2021-04-29 11:48:33 +0900

    Packaging ftc project (#26)
    * Add __init__.py for packaging
    
    * Add __init__py to all module
    
    * Add setup.py
    
    * Modify setup.py
    
    * Fix a little type
    
    * Modify README.md
    
    * Reflect skim's comment

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# fault-tolerant-control
+# Flight Simulator for Fault-Tolerant-Control Algorithms
 Project for fault tolerant control
+
+## Installation
+```bash
+git clone git@github.com:fdcl-ftc/fault-tolerant-control.git
+cd fault-tolerant-control
+pip install -e .
+```
+
+In Python 3.7>, use the module as follows.
+```
+import ftc
+```
---
+++ setup.py
@@ -0,0 +1,26 @@
+import os
+
+from setuptools import find_packages, setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="ftc",
+    version="0.1.0",
+    url="https://github.com:fdcl-ftc/fault-tolerant-control.git",
+    author="SNU FDCL",
+    description="SNU FDCL FTC",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="",
+    include_package_data=True,
+    packages=find_packages(),
+    python_requires=">=3.7",
+    install_requires=[
+        "fym",
+        "matplotlib"
+    ]
+)
