bc84951d7a36373c62aa655e2cad64c05ff5fa42
Hanna Lee <dlgkssk707@gmail.com>
2022-09-20 08:29:00 +0900

    Add LC62 model (#132)
    * Add LC62 model
    
    * Modify 6dof dyn
    
    * Fix a little
    
    * wip
    
    * Move safeupdate to utils
    
    * Add trim calc
    
    * Add VTOL trims
    
    * Fix polyfit error
    
    * Fix some bugs

--- examples/sample_controller_dsc.py
+++ examples/sample_controller_dsc.py
@@ -1,6 +1,4 @@
 import argparse
-from copy import deepcopy
-from functools import reduce
 
 import fym
 import matplotlib.pyplot as plt
@@ -9,30 +7,9 @@ from fym.utils.rot import hat
 from scipy.spatial.transform import Rotation
 
 import ftc
+from ftc.utils import safeupdate
 
 np.seterr(all="raise")
-
-
-def safeupdate(*configs):
-    assert len(configs) > 1
-
-    def _merge(base, new):
-        assert isinstance(base, dict), f"{base} is not a dict"
-        assert isinstance(new, dict), f"{new} is not a dict"
-        out = deepcopy(base)
-        for k, v in new.items():
-            # assert k in out, f"{k} not in {base}"
-            if isinstance(v, dict):
-                if "grid_search" in v:
-                    out[k] = v
-                else:
-                    out[k] = _merge(out[k], v)
-            else:
-                out[k] = v
-
-        return out
-
-    return reduce(_merge, configs)
 
 
 def cross(x, y):
--- ftc/utils.py
+++ ftc/utils.py
@@ -1,3 +1,6 @@
+from copy import deepcopy
+from functools import reduce
+
 from ftc.registration import registry
 
 
@@ -13,3 +16,25 @@ def get_controllers(*args, env=None):
         controllers.append(make(id, env=env))
 
     return controllers or [Controller() for Controller in registry.values()]
+
+
+def safeupdate(*configs):
+    assert len(configs) > 1
+
+    def _merge(base, new):
+        assert isinstance(base, dict), f"{base} is not a dict"
+        assert isinstance(new, dict), f"{new} is not a dict"
+        out = deepcopy(base)
+        for k, v in new.items():
+            # assert k in out, f"{k} not in {base}"
+            if isinstance(v, dict):
+                if "grid_search" in v:
+                    out[k] = v
+                else:
+                    out[k] = _merge(out[k], v)
+            else:
+                out[k] = v
+
+        return out
+
+    return reduce(_merge, configs)
