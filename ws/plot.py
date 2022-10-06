fbc2026dcfed60258d2ac2de05e6d99ca4041513
이지원 <godn@FDCL-FTC-MacBookAirui-MacBookAir.local>
2022-10-06 19:35:50 +0900

    add ws

--- ws/plot.py
+++ ws/plot.py
@@ -2,8 +2,15 @@ import fym
 import matplotlib.pyplot as plt
 
 data = fym.load("data.h5")
+plt.figure()
+plt.ylabel("position")
+plt.xlabel("time")
+plt.grid("True",'both')
 plt.plot(data["t"], data["pos"].squeeze())
 plt.figure()
+plt.xlabel("time")
+plt.ylabel("velocity")
+plt.grid("True",'both')
 plt.plot(data["t"], data["vel"].squeeze())
 
 plt.show()
