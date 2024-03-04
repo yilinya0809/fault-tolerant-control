8c9e9e67b049fa6baed496debe5490ac36e9ba7a
yilinya0809 <leejwblack@snu.ac.kr>
2024-03-04 15:26:47 +0900

    wip
    wip
    
    wip
    
    wip

--- ftc/models/LC62S.py
+++ ftc/models/LC62S.py
@@ -24,9 +24,14 @@ class LC62:
         self.th_r_max = 159.2089
         self.th_p_max = 91.5991
         X_trim, U_trim = self.get_trim(fixed={"h": 50, "VT": 45})
+        X_hover, U_hover = self.get_trim(fixed={"h": 50, "VT": 0})
+        # self.A, self.B = self.deriv_mat(X_hover[1:], U_hover, q=0)
 
         self.X_trim = X_trim[1:]
         self.U_trim = U_trim
+
+
+        # self.A , self.B = self.linearize(fixed={"h": 50, "VT": 0}, q=0)
     # def linearization(self, statefunc, states, ctrls, ptrb):
     #     n = np.size(states)
     #     m = np.size(ctrls)
@@ -122,7 +127,6 @@ class LC62:
             [0,1,0],
         )) * q
         
-       
         return A, B
 
     def deriv_lin(self, x, u, q):
@@ -150,16 +154,26 @@ class LC62:
         return Fx, Fz
 
 
-    def B_Fuselage_lin(self, vel):
+    def B_Fuselage_lin(self):
         S = self.S
         rho = 1.225
-        u, w = vel[0], vel[1]
+        u, w = self.X_trim[1], self.X_trim[2]
+        alp = self.U_trim[2]
+        # u, w = vel[0], vel[1]
         VT = norm_2(vel)
-        alp = arctan2(w, u)
+        VT = 45
+        # alp = arctan2(w, u)
         qbar = 0.5 * rho * VT**2
-        CL, CD = self.aero_coeff_lin(alp)
-        Fx = qbar * S * (CL * sin(alp) - CD * cos(alp))
-        Fz = -qbar * S * (CL*cos(alp) + CD * sin(alp))
+        
+        # CL, CD = self.aero_coeff_lin(alp)
+        # Fx = qbar * S * (CL * sin(alp) - CD * cos(alp))
+        # Fz = -qbar * S * (CL*cos(alp) + CD * sin(alp))
+        
+        CL = 0.2764 * alp - 0.0779
+        CD = 1.0075 * alp - 0.0121
+        Fx = qbar * S * (-0.0779 * alp -1.0075 * alp + 0.0121)
+        Fz = qbar * S * (0.2764 * alp - 0.0779 + 1.0075 * alp)
+
         return Fx, Fz
 
     def aero_coeff_lin(self, alp):
@@ -267,7 +281,10 @@ class LC62:
 
 if __name__ == "__main__":
     sys = LC62()
+    # print(sys.A)
+    # print(sys.B)
     
     Xdot1 = sys.derivnox(sys.X_trim, sys.U_trim, q=0)
-    Xdot2 = sys.deriv_lin(np.zeros((3,1)), np.zeros((3,1)), q=0)
-    print(Xdot1, Xdot2)
+    # Xdot2 = sys.deriv_lin(np.zeros((3,1)), np.zeros((3,1)), q=0)
+    breakpoint()
+    # print(Xdot1, Xdot2)
