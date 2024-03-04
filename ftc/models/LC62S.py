4c6ab58412d34d4ad6210c517b7704206b0c578b
yilinya0809 <leejwblack@snu.ac.kr>
2024-03-04 15:26:47 +0900

    wip
    wip
    
    wip
    
    wip

--- ftc/models/LC62S.py
+++ ftc/models/LC62S.py
@@ -1,7 +1,9 @@
+import numpy as np
 import scipy
 from casadi import *
+
 from ftc.utils import linearization
-import numpy as np
+
 
 class LC62:
     def __init__(self):
@@ -23,9 +25,14 @@ class LC62:
         self.th_r_max = 159.2089
         self.th_p_max = 91.5991
         X_trim, U_trim = self.get_trim(fixed={"h": 50, "VT": 45})
+        X_hover, U_hover = self.get_trim(fixed={"h": 50, "VT": 0})
+        # self.A, self.B = self.deriv_mat(X_hover[1:], U_hover, q=0)
 
         self.X_trim = X_trim[1:]
         self.U_trim = U_trim
+
+
+        # self.A , self.B = self.linearize(fixed={"h": 50, "VT": 0}, q=0)
     # def linearization(self, statefunc, states, ctrls, ptrb):
     #     n = np.size(states)
     #     m = np.size(ctrls)
@@ -104,41 +111,36 @@ class LC62:
         Fx, Fz = self.B_Fuselage(vel)
         dz = -sin(theta) * vel[0] + cos(theta) * vel[1]
         dvel = vertcat(
-            (Fp + Fx) / m - g * sin(theta)
-            (Fr + Fz) / m + g * cos(theta)
+            (Fp + Fx) / m - g * sin(theta),
+            (Fr + Fz) / m + g * cos(theta),
         )
         return vertcat(dz, dvel)
 
 
     
-    def deriv_mat(self, x, u):
+    def deriv_mat(self, x, u, q):
         f = self.derivnoxq
         ptrb = 1e-6
-        breakpoint()
-        A, B = linearization(f, x, u, ptrb)
+        A0, B = linearization(f, x, u, ptrb)
+        A = A0 + np.array((
+            [0,0,0],
+            [0,0,-1],
+            [0,1,0],
+        )) * q
+        
         
         return A, B
 
     def deriv_lin(self, x, u, q):
-        # A, B = self.deriv_mat(x, u)
-        n = MX.size(x)[0]
-        m = MX.size(u)[0]
+        AA, BB = self.deriv_mat(x, u, q)
+        # n = MX.size(x)[0]
+        # m = MX.size(u)[0]
 
-        A = np.zeros((n, n))
-        B = np.zeros((n, m))
+        # A = np.zeros((n, n))
+        # B = np.zeros((n, m))
         
-
         
-
-
-
-        self.A = A + np.array((
-            [0,0,0],
-            [0,0,-1],
-            [0,1,0],
-        )) @ q
-        self.B = B
-        Xdot = self.A @ x + self.B @ u
+        Xdot = AA @ x + BB @ u
         return Xdot
        
     def B_Fuselage(self, vel):
@@ -154,16 +156,26 @@ class LC62:
         return Fx, Fz
 
 
-    def B_Fuselage_lin(self, vel):
+    def B_Fuselage_lin(self):
         S = self.S
         rho = 1.225
-        u, w = vel[0], vel[1]
+        u, w = self.X_trim[1], self.X_trim[2]
+        alp = self.U_trim[2]
+        # u, w = vel[0], vel[1]
         VT = norm_2(vel)
-        alp = arctan2(w, u)
+        VT = 45
+        # alp = arctan2(w, u)
         qbar = 0.5 * rho * VT**2
-        CL, CD = self.aero_coeff_lin(alp)
-        Fx = qbar * S * (CL * sin(alp) - CD * cos(alp))
-        Fz = -qbar * S * (CL*cos(alp) + CD * sin(alp))
+        
+        # CL, CD = self.aero_coeff_lin(alp)
+        # Fx = qbar * S * (CL * sin(alp) - CD * cos(alp))
+        # Fz = -qbar * S * (CL*cos(alp) + CD * sin(alp))
+        
+        CL = 0.2764 * alp - 0.0779
+        CD = 1.0075 * alp - 0.0121
+        Fx = qbar * S * (-0.0779 * alp -1.0075 * alp + 0.0121)
+        Fz = qbar * S * (0.2764 * alp - 0.0779 + 1.0075 * alp)
+
         return Fx, Fz
 
     def aero_coeff_lin(self, alp):
@@ -245,9 +257,36 @@ class LC62:
         weight = np.diag([1, 1])
         return dvel.T @ weight @ dvel
 
+    def linearize(self, fixed={"h": 50, "VT": 0}, q=0):
+        h, VT = list(fixed.values())
+        # hovering setpoint
+        z = -h
+        Vx = VT
+        Vz = 0
+        Fr = -self.m * self.g
+        Fp = 0
+        theta = 0
+
+        X = np.vstack((z, Vx, Vz))
+        U = np.vstack((Fr, Fp, theta))
+        f = self.derivnoxq
+        ptrb = 1e-6
+        A0, B0 = linearization(f, X, U, ptrb)
+        A = A0 + np.array(([0, 0, 0], [0, 0, -1], [0, 1, 0])) * q
+        B = B0
+
+        return A, B
+
+
+
+
+
 if __name__ == "__main__":
     sys = LC62()
+    # print(sys.A)
+    # print(sys.B)
     
     Xdot1 = sys.derivnox(sys.X_trim, sys.U_trim, q=0)
-    Xdot2 = sys.deriv_lin(np.zeros((3,1)), np.zeros((3,1)), q=0)
-    print(Xdot1, Xdot2)
+    # Xdot2 = sys.deriv_lin(np.zeros((3,1)), np.zeros((3,1)), q=0)
+    breakpoint()
+    # print(Xdot1, Xdot2)
---
+++ ftc/models/LC62X.py
@@ -0,0 +1,142 @@
+import numpy as np
+import scipy
+from casadi import *
+
+from ftc.utils import linearization
+
+
+class LC62:
+    def __init__(self):
+
+        self.g = 9.81  # [m / sec^2]
+        self.m = 41.97  # [kg]
+        S1 = 0.2624  # [m^2]
+        S2 = 0.5898  # [m^2]
+        self.S = S1 + S2
+        self.tables = {
+            "alp": np.deg2rad(np.array([0, 2, 4, 6, 8, 10, 12, 14, 16])),  # [rad]
+            "CL": np.array(
+                [0.1931, 0.4075, 0.6112, 0.7939, 0.9270, 1.0775, 0.9577, 1.0497, 1.0635]
+            ),
+            "CD": np.array(
+                [0.0617, 0.0668, 0.0788, 0.0948, 0.1199, 0.1504, 0.2105, 0.2594, 0.3128]
+            ),
+        }
+        self.th_r_max = 159.2089
+        self.th_p_max = 91.5991
+        self.X_trim = np.vstack((0, 45, 0))
+        self.U_trim = np.vstack((0, 69.69042, 3.15312039e-02))
+        self.X_hover = np.vstack((-50, 0, 0))
+        self.U_hover = np.vstack((self.m * self.g, 0, 0))
+     
+    def aero_coeff_lin(self, alp):
+        CL = 0.2764 * alp - 0.0779
+        CD = 1.0075 * alp - 0.0121
+        return CL, CD
+
+    def aero_coeff(self, alp):
+        clgrid = interpolant(
+            "CLGRID", "bspline", [self.tables["alp"]], self.tables["CL"]
+        )
+        cdgrid = interpolant(
+            "CDGRID", "bspline", [self.tables["alp"]], self.tables["CD"]
+        )
+        CL = clgrid(alp)
+        CD = cdgrid(alp)
+        return CL, CD
+
+    def B_aero(self, VT, alp):
+        S = self.S
+        rho = 1.225
+        qbar = 0.5 * rho * VT**2
+        CL, CD = self.aero_coeff(alp)
+        L = qbar * S * CL
+        D = qbar * S * CD
+        return L, D
+
+
+    def deriv(self, X, U):
+        g = self.g
+        m = self.m
+        W = self.m * self.g
+        z, V, gamma = X[0], X[1], X[2]
+        Fr, Fp, alp = U[0], U[1], U[2]
+        L, D = self.B_aero(V, alp)
+
+        dz = -V * sin(gamma)
+        dV = (-D + Fp * cos(alp) - Fr * sin(alp) - W * sin(gamma)) / self.m
+        # if is_equal(V, 0, 1):
+        #     dgamma = 0
+        # else:
+        #     dgamma = (L + Fp * sin(alp) + Fr * cos(alp) - W *cos(gamma))/(self.m * V)
+        dgamma = (L + Fp * sin(alp) + Fr * cos(alp) - W *cos(gamma))/(self.m * V)
+
+        return vertcat(dz, dV, dgamma)
+
+    def get_trim(
+        self,
+        z0={
+            "alpha": 0.0,
+            "gamma": 0.0,
+            "Fr": 0,
+            "Fp": 0.5,
+        },
+        fixed={"h": 50, "VT": 45},
+        method="SLSQP",
+        options={"disp": False, "ftol": 1e-10},
+    ):
+        z0 = list(z0.values())
+        fixed = list(fixed.values())
+        bounds = (
+            np.deg2rad((0, 20)),
+            np.deg2rad((-20, 20)),
+            [0, 1000],
+            [0, 180],
+        )
+        result = scipy.optimize.minimize(
+            self._trim_cost,
+            z0,
+            args=(fixed,),
+            bounds=bounds,
+            method=method,
+            options=options,
+        )
+
+        h, VT = fixed
+        if np.isclose(VT, 0):
+            alp, gamma, Fr, Fp = 0, 0, self.m * self.g, 0
+        else:
+            alp, gamma, Fr, Fp = result.x
+
+        X_trim = np.vstack((-h, VT, gamma))
+        U_trim = np.vstack((Fr, Fp, alp))
+        return X_trim, U_trim
+
+    def _trim_cost(self, z, fixed):
+        h, VT = fixed
+        alp, gamma, Fr, Fp = z
+        # pos_trim = np.vstack((0, -h))
+        # vel_trim = np.vstack((VT * cos(gamma), -VT * sin(gamma)))
+        
+        X_trim = np.vstack((-h, VT, gamma))
+        U_trim = np.vstack((Fr, Fp, alp))
+        theta = gamma + alp
+
+        dX = self.deriv(X_trim, U_trim)
+        # Vth = np.vstack((dX[1], gamma))
+        weight = np.diag([1, 1, 1])
+        return dX.T @ weight @ dX
+
+
+
+if __name__ == "__main__":
+    sys = LC62()
+    # print(sys.A)
+    # print(sys.B)
+    
+    Xdot1 = sys.deriv(sys.X_trim, sys.U_trim)
+    Xdot2 = sys.deriv(sys.X_hover, sys.U_hover)
+    breakpoint()
+
+    # Xdot2 = sys.deriv_lin(np.zeros((3,1)), np.zeros((3,1)), q=0)
+    # print(Xdot1, Xdot2)
