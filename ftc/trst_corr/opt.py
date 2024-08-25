22b6844a365858ac35c621773e302f9cc34f9de4
yilinya0809 <leejwblack@snu.ac.kr>
2024-08-25 15:39:15 +0900

    Trajectory optimization considering transition corridor

--- ftc/trst_corr/opt.py
+++ ftc/trst_corr/opt.py
@@ -1,10 +1,67 @@
 import matplotlib.pyplot as plt
+import numpy as np
 from casadi import *
+from pylab import figure, legend, plot, show
 
+# q = 0
 from ftc.models.LC62_opt import LC62
+from ftc.trst_corr.poly_corr import boundary
 
-# from dyn import LC62
+Trst_corr = np.load("ftc/trst_corr/corr_safe.npz")
+VT_corr = Trst_corr["VT_corr"]
+theta_corr = np.rad2deg(Trst_corr["theta_corr"])
+cost = Trst_corr["cost"]
+success = Trst_corr["success"]
+upper_bound, lower_bound = boundary(Trst_corr)
 
+mask = lower_bound > np.min(lower_bound)
+VT_filtered = VT_corr[mask]
+lower_bound_filtered = lower_bound[mask]
+
+deg = 3
+upper = np.polyfit(VT_corr, upper_bound, deg)
+lower = np.polyfit(VT_filtered, lower_bound_filtered, deg)
+
+
+def casadi_polyval(coeffs, x):
+    value = 0
+    deg = len(coeffs) - 1
+    for i, coeff in enumerate(coeffs):
+        value += coeff * x ** (deg - i)
+    return value
+
+
+def upper_func(vel):
+    value = casadi_polyval(upper, vel)
+    return value
+
+
+def lower_func(vel):
+    value_min = np.min(lower_bound)
+    value_low = casadi_polyval(lower, vel)
+    value = if_else(vel < VT_filtered[0], value_min, value_low)
+    return value
+
+
+upper_data = np.zeros((np.size(VT_corr), 1))
+lower_data = np.zeros((np.size(VT_corr), 1))
+for i in range(np.size(VT_corr)):
+    upper_data[i] = upper_func(VT_corr[i])
+    lower_data[i] = lower_func(VT_corr[i])
+
+
+# fig, ax = plt.subplots(1, 1)
+# VT, theta = np.meshgrid(VT_corr, theta_corr)
+# ax.scatter(VT, theta, s=success.T, c="b")
+# ax.plot(VT_corr, np.rad2deg(upper_data))
+# ax.plot(VT_corr, np.rad2deg(lower_data))
+# ax.set_xlabel("VT, m/s", fontsize=15)
+# ax.set_ylabel(r"$\theta$, deg", fontsize=15)
+# ax.set_title("Dynamic Transition Corridor", fontsize=20)
+
+# plt.show()
+
+# breakpoint()
 
 plant = LC62()
 
@@ -47,6 +104,11 @@ for k in range(N):  # loop over control intervals
     x_next = X[:, k] + dt / 6 * (k1 + 2 * k2 + 2 * k3 + k4)
     opti.subject_to(X[:, k + 1] == x_next)  # close the gaps
 
+    # Transition Corridor
+    theta_k = U[2, k]
+    VT_k = norm_2(X[1:3, k])
+    opti.subject_to(opti.bounded(lower_func(VT_k), theta_k, upper_func(VT_k)))
+
 Fr_max = 6 * plant.th_r_max
 Fp_max = 2 * plant.th_p_max
 # ---- input constraints --------
@@ -80,52 +142,81 @@ opti.set_initial(vx, x_trim[2] / 2)
 opti.set_initial(vz, x_trim[3] / 2)
 opti.set_initial(Fr, plant.m * plant.g / 2)
 opti.set_initial(Fp, u_trim[1] / 2)
-opti.set_initial(theta, u_trim[2] / 2)
+opti.set_initial(theta, np.deg2rad(0))
 opti.set_initial(T, 20)
 
 # ---- solve NLP              ------
 opti.solver("ipopt")  # set numerical backend
-sol = opti.solve()  # actual solve
-
-# ---- post-processing        ------
-from pylab import figure, legend, plot, show
-
-tf = sol.value(T)
-tspan = linspace(0, tf, N + 1)
-
-""" States trajectory """
-fig, axs = plt.subplots(3, 2, squeeze=False, sharex=True)
-ax = axs[0, 0]
-ax.plot(tspan, -sol.value(z), "k")
-ax.set_ylabel("$h$, m")
-ax.grid()
-
-ax = axs[1, 0]
-ax.plot(tspan, sol.value(vx), "k")
-ax.set_ylabel("$V_x$, m/s")
-ax.grid()
-
-ax = axs[2, 0]
-ax.plot(tspan, sol.value(vz), "k")
-ax.set_ylabel("$V_z$, m/s")
-ax.set_xlabel("Time, s")
-ax.grid()
-
-ax = axs[0, 1]
-ax.plot(tspan[:-1], sol.value(Fr), "k")
-ax.set_ylabel("Rotor, N")
-ax.grid()
-
-ax = axs[1, 1]
-ax.plot(tspan[:-1], sol.value(Fp), "k")
-ax.set_ylabel("Pusher, N")
-ax.grid()
-
-ax = axs[2, 1]
-ax.plot(tspan[:-1], np.rad2deg(sol.value(theta)), "k")
-ax.set_ylabel(r"$\theta$, m/s")
-ax.set_xlabel("Time, s")
-ax.grid()
+results = {}
 
 
-plt.show()
+def plot_results(data):
+    tspan = linspace(0, data["tf"], N + 1)
+
+    """ States trajectory """
+    fig, axs = plt.subplots(3, 2, squeeze=False, sharex=True)
+    ax = axs[0, 0]
+    ax.plot(tspan, -data["X"][0, :], "k")
+    ax.set_ylabel("$h$, m")
+    ax.grid()
+
+    ax = axs[1, 0]
+    ax.plot(tspan, data["X"][1, :], "k")
+    ax.set_ylabel("$V_x$, m/s")
+    ax.grid()
+
+    ax = axs[2, 0]
+    ax.plot(tspan, data["X"][2, :], "k")
+    ax.set_ylabel("$V_z$, m/s")
+    ax.set_xlabel("Time, s")
+    ax.grid()
+
+    ax = axs[0, 1]
+    ax.plot(tspan[:-1], data["U"][0, :], "k")
+    ax.set_ylabel("Rotor, N")
+    ax.grid()
+
+    ax = axs[1, 1]
+    ax.plot(tspan[:-1], data["U"][1, :], "k")
+    ax.set_ylabel("Pusher, N")
+    ax.grid()
+
+    ax = axs[2, 1]
+    ax.plot(tspan[:-1], np.rad2deg(data["U"][2, :]), "k")
+    ax.set_ylabel(r"$\theta$, m/s")
+    ax.set_xlabel("Time, s")
+    ax.grid()
+
+    """ VT, theta traj """
+    fig, ax = plt.subplots(1, 1)
+    VT_traj = np.zeros((N, 1))
+    theta_traj = np.zeros((N, 1))
+    for i in range(N):
+        VT_traj[i] = norm_2(data["X"][1:3, i])
+        theta_traj[i] = np.rad2deg(data["U"][2, i])
+
+    ax.plot(VT_traj, theta_traj, "r--")
+    VT, theta = np.meshgrid(VT_corr, theta_corr)
+    ax.scatter(VT, theta, s=success.T, c="b")
+    ax.set_xlabel("VT, m/s", fontsize=15)
+    ax.set_ylabel(r"$\theta$, deg", fontsize=15)
+    ax.set_title("Dynamic Transition Corridor", fontsize=20)
+
+    plt.show()
+
+
+try:
+    sol = opti.solve()  # actual solve
+    results["tf"] = sol.value(T)
+    results["X"] = sol.value(X)
+    results["U"] = sol.value(U)
+    plot_results(results)
+
+
+except RuntimeError as e:
+    print("Solver Failed!")
+    results["tf"] = float(opti.debug.value(T))
+    results["X"] = opti.debug.value(X)
+    results["U"] = opti.debug.value(U)
+
+    plot_results(results)
--- ftc/trst_corr/opt2.py
+++ ftc/trst_corr/opt2.py
@@ -4,8 +4,65 @@ from casadi import *
 from scipy.optimize import curve_fit
 
 import ftc
+# consider q as control input
 from ftc.models.LC62_optq import LC62
 from ftc.trst_corr.poly_corr import boundary
+
+Trst_corr = np.load("ftc/trst_corr/corr_safe.npz")
+VT_corr = Trst_corr["VT_corr"]
+theta_corr = np.rad2deg(Trst_corr["theta_corr"])
+cost = Trst_corr["cost"]
+success = Trst_corr["success"]
+upper_bound, lower_bound = boundary(Trst_corr)
+
+mask = lower_bound > np.min(lower_bound)
+VT_filtered = VT_corr[mask]
+lower_bound_filtered = lower_bound[mask]
+
+deg = 3
+upper = np.polyfit(VT_corr, upper_bound, deg)
+lower = np.polyfit(VT_filtered, lower_bound_filtered, deg)
+
+
+def casadi_polyval(coeffs, x):
+    value = 0
+    deg = len(coeffs) - 1
+    for i, coeff in enumerate(coeffs):
+        value += coeff * x ** (deg - i)
+    return value
+
+
+def upper_func(vel):
+    value = casadi_polyval(upper, vel)
+    return value
+
+
+def lower_func(vel):
+    value_min = np.min(lower_bound)
+    value_low = casadi_polyval(lower, vel)
+    value = if_else(vel < VT_filtered[0], value_min, value_low)
+    return value
+
+
+upper_data = np.zeros((np.size(VT_corr), 1))
+lower_data = np.zeros((np.size(VT_corr), 1))
+for i in range(np.size(VT_corr)):
+    upper_data[i] = upper_func(VT_corr[i])
+    lower_data[i] = lower_func(VT_corr[i])
+
+
+# fig, ax = plt.subplots(1, 1)
+# VT, theta = np.meshgrid(VT_corr, np.rad2deg(theta_corr))
+# ax.scatter(VT, theta, s=success.T, c="b")
+# ax.plot(VT_corr, np.rad2deg(upper_data))
+# ax.plot(VT_corr, np.rad2deg(lower_data))
+# ax.set_xlabel("VT, m/s", fontsize=15)
+# ax.set_ylabel(r"$\theta$, deg", fontsize=15)
+# ax.set_title("Dynamic Transition Corridor", fontsize=20)
+
+# plt.show()
+
+# breakpoint()
 
 plant = LC62()
 
@@ -34,7 +91,7 @@ q = U[2, :]
 T = opti.variable()
 
 # ---- objective          ---------
-W = diag([1, 1, 0])
+W = diag([1, 1, 10000])
 obj = dot(U, W @ U)
 opti.minimize(obj)
 
@@ -48,11 +105,11 @@ for k in range(N):  # loop over control intervals
     x_next = X[:, k] + dt / 6 * (k1 + 2 * k2 + 2 * k3 + k4)
     opti.subject_to(X[:, k + 1] == x_next)  # close the gaps
 
-    # tht = U[2, k]
-    # VT = norm_2(X[1:, k])
-    # opti.subject_to(
-    #     opti.bounded(lower_func(VT), theta, upper_func(VT))
-    # )  # close the gaps
+    # Transition Corridor
+    theta_k = X[3, k]
+    VT_k = norm_2(X[1:3, k])
+    opti.subject_to(opti.bounded(lower_func(VT_k), theta_k, upper_func(VT_k)))
+
 
 Fr_max = 6 * plant.th_r_max
 Fp_max = 2 * plant.th_p_max
@@ -89,7 +146,7 @@ opti.subject_to(q[-1] == U_target[2])
 opti.set_initial(z, X_target[0])
 opti.set_initial(vx, X_target[1] / 2)
 opti.set_initial(vz, X_target[2] / 2)
-opti.set_initial(theta, X_target[2] / 2)
+opti.set_initial(theta, X_target[3] / 2)
 opti.set_initial(Fr, plant.m * plant.g / 2)
 opti.set_initial(Fp, U_target[1] / 2)
 opti.set_initial(q, U_target[2] / 2)
@@ -97,91 +154,76 @@ opti.set_initial(T, 20)
 
 # ---- solve NLP              ------
 opti.solver("ipopt")  # set numerical backend
-sol = opti.solve()  # actual solve
-
-# ---- post-processing        ------
-from pylab import figure, grid, legend, plot, show, subplot, xlabel, ylabel
-
-tf = sol.value(T)
-tspan = linspace(0, tf, N + 1)
-cost = sol.value(obj)
-
-""" States trajectory """
-fig, axs = plt.subplots(3, 2, squeeze=False, sharex=True)
-ax = axs[0, 0]
-ax.plot(tspan, -sol.value(z), "k")
-ax.set_ylabel("$h$, m")
-ax.grid()
-
-ax = axs[1, 0]
-ax.plot(tspan, sol.value(vx), "k")
-ax.set_ylabel("$V_x$, m/s")
-ax.grid()
-
-ax = axs[2, 0]
-ax.plot(tspan, sol.value(vz), "k")
-ax.set_ylabel("$V_z$, m/s")
-ax.set_xlabel("Time, s")
-ax.grid()
-
-ax = axs[0, 1]
-ax.plot(tspan[:-1], sol.value(Fr), "k")
-ax.set_ylabel("Rotor, N")
-ax.grid()
-
-ax = axs[1, 1]
-ax.plot(tspan[:-1], sol.value(Fp), "k")
-ax.set_ylabel("Pusher, N")
-ax.grid()
-
-ax = axs[2, 1]
-ax.plot(tspan, np.rad2deg(sol.value(theta)), "k")
-ax.set_ylabel(r"$\theta$, m/s")
-ax.set_xlabel("Time, s")
-ax.grid()
+results = {}
 
 
-""" Cost trajectory """
-fig, ax = plt.subplots(1, 1)
-# ax.plot(tspan[:-1], cost, "k")
-ylabel("Cost")
-xlabel("Time, s")
-grid()
+def plot_results(data):
+    tspan = linspace(0, data["tf"], N + 1)
+
+    """ States trajectory """
+    fig, axs = plt.subplots(3, 2, squeeze=False, sharex=True)
+    ax = axs[0, 0]
+    ax.plot(tspan, -data["X"][0, :], "k")
+    ax.set_ylabel("$h$, m")
+    ax.grid()
+
+    ax = axs[1, 0]
+    ax.plot(tspan, data["X"][1, :], "k")
+    ax.set_ylabel("$V_x$, m/s")
+    ax.grid()
+
+    ax = axs[2, 0]
+    ax.plot(tspan, data["X"][2, :], "k")
+    ax.set_ylabel("$V_z$, m/s")
+    ax.set_xlabel("Time, s")
+    ax.grid()
+
+    ax = axs[0, 1]
+    ax.plot(tspan[:-1], data["U"][0, :], "k")
+    ax.set_ylabel("Rotor, N")
+    ax.grid()
+
+    ax = axs[1, 1]
+    ax.plot(tspan[:-1], data["U"][1, :], "k")
+    ax.set_ylabel("Pusher, N")
+    ax.grid()
+
+    ax = axs[2, 1]
+    ax.plot(tspan, np.rad2deg(data["X"][3, :]), "k")
+    ax.set_ylabel(r"$\theta$, m/s")
+    ax.set_xlabel("Time, s")
+    ax.grid()
+
+    """ VT, theta traj """
+    fig, ax = plt.subplots(1, 1)
+    VT_traj = np.zeros((N, 1))
+    theta_traj = np.zeros((N, 1))
+    for i in range(N):
+        VT_traj[i] = norm_2(data["X"][1:3, i])
+        theta_traj[i] = np.rad2deg(data["X"][3, i])
+
+    ax.plot(VT_traj, theta_traj, "r--")
+    VT, theta = np.meshgrid(VT_corr, theta_corr)
+    ax.scatter(VT, theta, s=success.T, c="b")
+    ax.set_xlabel("VT, m/s", fontsize=15)
+    ax.set_ylabel(r"$\theta$, deg", fontsize=15)
+    ax.set_title("Dynamic Transition Corridor", fontsize=20)
+
+    plt.show()
 
 
-""" Corridor trajectory """
-# vx = sol.value(vx)
-# vz = sol.value(vz)
-
-# VT_traj = np.zeros((np.size(tspan), 1))
-# theta_traj = np.rad2deg(sol.value(theta))
-# for i in range(np.size(tspan)):
-#     VT_traj[i] = np.linalg.norm(vx[i], vz[i])
+try:
+    sol = opti.solve()  # actual solve
+    results["tf"] = sol.value(T)
+    results["X"] = sol.value(X)
+    results["U"] = sol.value(U)
+    plot_results(results)
 
 
-# fig, ax = plt.subplots(1, 1)
-# ax.plot(VT_traj, theta_traj, "k")
-# ylabel("VT, m/s")
-# xlabel(r"$\theta$, deg")
-# grid()
+except RuntimeError as e:
+    print("Solver Failed!")
+    results["tf"] = float(opti.debug.value(T))
+    results["X"] = opti.debug.value(X)
+    results["U"] = opti.debug.value(U)
 
-
-# """ Corridor """
-# Trst_corr = np.load("ftc/trst_corr/corr_safe.npz")
-# VT_corr = Trst_corr["VT_corr"]
-# acc_corr = Trst_corr["acc"]
-# theta_corr = np.rad2deg(Trst_corr["theta_corr"])
-# Fr = Trst_corr["Fr"]
-# Fp = Trst_corr["Fp"]
-
-# upper_bound, lower_bound = boundary(Trst_corr)
-# VT, theta = np.meshgrid(VT_corr, theta_corr)
-
-# upper = upper_func(VT_corr)
-# lower = lower_func(VT_corr)
-# fig, ax = plt.subplots(1, 1)
-# ax.plot(VT_corr, upper, "r--", label="upper function")
-# ax.plot(VT_corr, lower, "b--", label="lower function")
-
-
-show()
+    plot_results(results)
